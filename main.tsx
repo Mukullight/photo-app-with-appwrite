fcf7262fa53a1695d7e3b0ab866c19e1044b847a
Mukul Namagiri <81064535+Mukullight@users.noreply.github.com>
2024-04-15 18:08:48 +0100

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowerRouter} from 'react-router-dom'
+import App from './app';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <BrowerRouter> <App /></BrowerRouter>
+)
+
+
+
